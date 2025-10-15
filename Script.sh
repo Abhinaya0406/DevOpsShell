0756e577dd61979001247d88aa95805d90067862
Abhinaya0406 <abhinayasarika2010@gmail.com>
2025-10-15 11:30:01 -0400

    sql

--- Script.sh
+++ Script.sh
@@ -4,7 +4,17 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then 
-echo "not root user"
+echo "not root user - Please try with root user"
+exit 1
 else 
 echo "Root User"
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0]
+then
+echo "ERROR Installing"
+else 
+echo "success"
 fi
