7d92a22f36cf7e1eeb950b0671f3c9d433ae0ca2
Abhinaya0406 <abhinayasarika2010@gmail.com>
2025-10-16 11:15:12 -0400

    added loops

--- loops.sh
+++ loops.sh
@@ -18,13 +18,27 @@ if [ $1 -ne 0 ]
 then
 echo "Already Installaed $2">$LOG_FILE
 else
-echo "Installing $2">$LOG_FILE
+echo "FAILED $2">$LOG_FILE
 fi
 }
 
-for each $PACKAGE in $@
+for PACKAGE in $@
+
 do
 echo $PACKAGE
+
+yum list installed $PACKAGE
+
+if [ $? -ne 0 ]
+then
 yum install $PACKAGE -y>$LOG_FILE
-VALIDATE $? $PACKAGE>$LOG_FILE
+VALIDATE() $? $PACKAGE>$LOG_FILE
+else
+echo "$PACKAGE Already Installed"
+fi
 done
+
+
+
+
+
