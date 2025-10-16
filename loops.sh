55cfdf3fd650eb4cf390193254255ad3e384a43a
Abhinaya0406 <abhinayasarika2010@gmail.com>
2025-10-16 18:16:42 -0400

    added loops

--- loops.sh
+++ loops.sh
@@ -5,40 +5,33 @@ TIMESTAMP=$(date +%F-%H-%M)
 LOG_FILE="/tmp/$0_$TIMESTAMP.log"
 
 if [ $ID -ne 0 ]
-then
-echo "Please try with root access"
-exit 1
-else
-echo "Root User"
+	then
+		echo "Please try with root access"
+		exit 1
+	else
+		echo "Root User"
 fi
 
 
 VALIDATE(){
 if [ $1 -ne 0 ]
-then
-echo "SUCCESS $2" &>>$LOG_FILE
-else
-echo "FAILED $2" &>>$LOG_FILE
+	then
+		echo "SUCCESS $2" &>>$LOG_FILE
+	else
+		echo "FAILED $2" &>>$LOG_FILE
 fi
 }
 
 for PACKAGE in $@
-
 do
-echo $PACKAGE
-
-yum list installed $PACKAGE &>>$LOG_FILE
-
-if [ $? -ne 0 ]
-then
-echo "installing $PACKAGE"
-yum install $PACKAGE -y &>>$LOG_FILE
-
-VALIDATE $? $PACKAGE &>>$LOG_FILE
-
-else
-
-echo "already installed--skipping $PACKAGE"
+	yum list installed $PACKAGE &>>$LOG_FILE
+	
+		if [ $? -ne 0 ]
+			then
+				yum install $PACKAGE -y &>>$LOG_FILE
+				VALIDATE $? $PACKAGE &>>$LOG_FILE
+		else
+				echo "already installed--skipping $PACKAGE"
 fi
 done
 
